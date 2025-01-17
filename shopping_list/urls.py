f3789f80e7bfe2cfce66670af312f6f3b0a0fc0b
Justin <thatdevjustin@gmail.com>
2025-01-17 17:08:55 -0500

    Add pytest configuration and implement tests for ShoppingList and ShoppingItem

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -6,6 +6,6 @@ from shopping_list.views import AddShoppingItem, ListAddShoppingList, ShoppingIt
 urlpatterns = [
     path("api/shopping-lists/", ListAddShoppingList.as_view(), name="all_shopping_lists"),
     path("api/shopping-list/<uuid:pk>/", ShoppingListDetail.as_view(), name="shopping_list_detail"),
-    path("api/shopping-lists/<uuid:pk>/shopping-items/", AddShoppingItem.as_view(), name="add-shopping-item"),
-    path("api/shopping-lists/<uuid:pk>/shopping-items/<uuid:item_pk>/", ShoppingItemDetail.as_view(), name="shopping-item-detail"),
+    path("api/shopping-lists/<uuid:pk>/shopping-items/", AddShoppingItem.as_view(), name="add_shopping_item"),
+    path("api/shopping-lists/<uuid:pk>/shopping-items/<uuid:item_pk>/", ShoppingItemDetail.as_view(), name="shopping_item_detail"),
 ]
